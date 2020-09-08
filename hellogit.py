3cf50adfb0ed9d97dabea5ba121dd21d9f4701d4
imamrdn <imamrmdn99@gmail.com>
2020-09-08 10:06:00 +0700

    menambahkan fungsi

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,11 @@
 def my_function():
 print("Hello Github!")
+
+def my_function():
+  print("Ini adalah fungsi pertama")
+
+def my_function2():
+  print("Ini adalah fungsi kedua")
+
+def my_function3():
+  print("Ini adalah fungsi ketiga")
